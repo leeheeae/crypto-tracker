26c409a4a5a9af88dd9b444bf81fbd0fa1ff966a
leeheeae <chromeee@daum.net>
2022-05-15 15:11:08 +0900

    기본 설정

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { ThemeProvider } from 'styled-components'
 import reportWebVitals from './reportWebVitals';
+import { lightTheme, darkTheme } from './theme';
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={lightTheme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
