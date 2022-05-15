7d94113b6924ff087a8505798471545b9db90af1
leeheeae <chromeee@daum.net>
2022-05-15 15:15:47 +0900

    Router설정 (파일 분리하여 적용해보기)

---
+++ src/routes/Coin.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Coin = () => {
+    return (
+        <div>
+            코인
+        </div>
+    );
+};
+
+export default Coin;
---
+++ src/routes/Coins.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Coins = () => {
+    return (
+        <div>
+            코인스
+        </div>
+    );
+};
+
+export default Coins;
