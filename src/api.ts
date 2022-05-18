f378208d0bd30c8f8b28e05c8fd64c10b6c41c5c
leeheeae <chromeee@daum.net>
2022-05-18 17:41:39 +0900

    chart

--- src/api.ts
+++ src/api.ts
@@ -11,3 +11,9 @@ export const fetchCoinsInfo = (coinId: string | undefined) => {
 export const fetchCoinsTickers = (coinId: string | undefined) => {
     return fetch(`${BASE_URL}/tickers/${coinId}`).then(response => response.json());
 }
+
+export const fetchCoinHistory = (coinId: string | undefined) => {
+    const endDate = Math.floor(Date.now() / 1000);
+    const startDate = endDate - 60 * 60 * 24 * 7 * 2;
+    return fetch(`${BASE_URL}/coins/${coinId}/ohlcv/historical?start=${startDate}&end=${endDate}`).then(response => response.json());
+}
--- src/routes/Chart.tsx
+++ src/routes/Chart.tsx
@@ -1,9 +1,15 @@
-const Chart = () => {
-    return (
-        <div>
-            차트
-        </div>
-    );
+import { useQuery } from 'react-query';
+import { fetchCoinHistory } from '../api';
+
+interface ChartProps {
+  coinId: string | undefined;
+}
+
+const Chart = ({ coinId }: ChartProps) => {
+  const { isLoading, data: endData } = useQuery(['ohlcv', coinId], () =>
+    fetchCoinHistory(coinId),
+  );
+  return <div>차트</div>;
 };
 
 export default Chart;
